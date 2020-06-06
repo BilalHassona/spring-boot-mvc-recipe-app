5a942a76e8952f82ae91db6a3bd4cc3ccdfd787f
Bilal <bilal.hassona@gmail.com>
2020-06-06 15:47:26 +0200

    Init commit

---
+++ src/main/java/springbootrecipeapp/repositories/RecipeRepository.java
@@ -0,0 +1,10 @@
+package springbootrecipeapp.repositories;
+
+import springbootrecipeapp.domain.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by jt on 6/13/17.
+ */
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+}
---
+++ src/main/java/springbootrecipeapp/services/UnitOfMeasureService.java
@@ -0,0 +1,13 @@
+package springbootrecipeapp.services;
+
+import springbootrecipeapp.commands.UnitOfMeasureCommand;
+
+import java.util.Set;
+
+/**
+ * Created by jt on 6/28/17.
+ */
+public interface UnitOfMeasureService {
+
+    Set<UnitOfMeasureCommand> listAllUoms();
+}
